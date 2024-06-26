d67fc217611dcfb200416a46362203dbb28adcb2
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-06-26 21:53:04 +0300

    "Adds a new function 'add' that takes two integers and returns their sum."

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * adds two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
