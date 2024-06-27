86af44e62b0ea632877bd816aa403e39b09c2a95
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-06-27 22:07:37 +0300

    Edit line betty

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void more_numbers(void)
+{
+int i, num;
+
+for (i = 0; i < 10; i++)
+{
+for (num = 0; num <= 14; num++)
+{
+if (num >= 10)
+_putchar('1');
+_putchar(num % 10 + '0');
+}
+_putchar('\n');
+}
+}
