e50bdc7d8cf2cecc358037b5149602f612269ba4
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-06-27 22:20:23 +0300

    "Added function documentation comment to _isdigit function"

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * _isdigit - Checks for a digit (0 through 9)
+ * @c: The character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
 int _isdigit(int c)
 {
 if (c >= '0' && c <= '9')
