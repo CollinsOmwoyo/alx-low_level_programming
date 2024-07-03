8fc6036c7ca2941ecb087d8c1b839ef4e18ac580
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-07-03 09:05:11 +0300

    Made code optimization from AI Tool

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[i])
+{
+/* I am using this to check if the current character is a separator code frm AI Tool */
+if (i == 0 || (str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+str[i - 1] == '}'))
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 'a' + 'A';
+}
+}
+i++;
+}
+
+return str;
+}
