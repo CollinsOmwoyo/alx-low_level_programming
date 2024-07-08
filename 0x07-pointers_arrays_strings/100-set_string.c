61ecf65f874fb08427fbf8a043d4ce82e838ab3f
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-07-08 22:12:19 +0300

    Additional task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to be set
+ * @to: char to set the pointer to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
