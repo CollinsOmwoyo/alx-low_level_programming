15c1713490cea20f1ba9061a710798bad66e30f1
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-07-16 21:10:43 +0300

    "Added 0-whatsmyname.c file with main function that prints the program name"

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
