42113b228795f775bc03fe0e043ae484511f79f7
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-07-26 23:49:35 +0300

    "Added function documentation comment to print_name function"

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
-
+/**
+ * print_name - Prints a name using a function pointer.
+ * @name: The name to print.
+ * @f: A pointer to the function used to print the name.
+ *
+ * Description: This function calls the function pointed to by f
+ *               to print the given name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 if (name && f)
