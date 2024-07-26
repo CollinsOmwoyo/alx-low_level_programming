0fc70b4a0bf3466bade03d42a19b960fa526bc73
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-07-26 23:52:57 +0300

    Added function documentation for get_op_func in 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,17 @@
 #include "3-calc.h"
-
+/**
+ * get_op_func - Selects the correct function to perform the operation
+ *                requested by the user.
+ * @s: The operator.
+ *
+ * Return: A pointer to the function that corresponds to the operator
+ *         given as a parameter. Returns NULL if the operator is not
+ *         recognized.
+ *
+ * Description: This function maps the operator string to the corresponding
+ *               function. It iterates through a predefined list of operators
+ *               and functions and returns the appropriate function pointer.
+ */
 int op_add(int a, int b) { return a + b; }
 int op_sub(int a, int b) { return a - b; }
 int op_mul(int a, int b) { return a * b; }
