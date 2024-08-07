9ad77ba10aa2ddfc99111e7d5ccd190b4cae1401
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-08-07 11:04:41 +0300

    "Refactored add_node_end function: added parameter documentation, changed error handling, and reformatted code."

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,47 +1,51 @@
 #include "lists.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * add_node_end - Adds a new node at the end of a list_t list.
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: pointer to the pointer to the head of the list.
+ * @str: string to be added to the new node.
  *
- * @head: Double pointer to the head of the list.
- * @str: String to be added to the new node.
- * Return: Address of the new element, or NULL if it failed.
+ * Return: the address of the new element, or NULL if it failed.
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new_node;
 list_t *temp = *head;
-size_t len;
-
-new_node = (list_t *)malloc(sizeof(list_t));
+char *dup_str;
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-exit(1);
-
-new_node->str = strdup(str);
-if (new_node->str == NULL)
 {
-free(new_node);
-exit(1);
+return (NULL);
 }
 
-for (len = 0; str[len]; len++)
-;
+/*Duplicate the string and check for success*/
+dup_str = strdup(str);
+if (dup_str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-new_node->len = len;
+/*Initialize the new node*/
+new_node->str = dup_str;
+new_node->len = strlen(dup_str);
 new_node->next = NULL;
-
 if (*head == NULL)
 {
 *head = new_node;
 }
 else
 {
+
 while (temp->next != NULL)
+{
 temp = temp->next;
+}
+/*Add the new node at the end*/
 temp->next = new_node;
 }
 
-return new_node;
+return (new_node);
 }
