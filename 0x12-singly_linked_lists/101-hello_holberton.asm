ea712d7979b2ce84c9256864b660bfe1a60c6edb
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-08-07 11:21:38 +0300

    "Updated 101-hello_holberton.asm: added null-terminator to msg, changed printf call to use extern function, and reformatted code with consistent spacing."

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,20 @@
 section .data
-msg db 'Hello, Holberton', 10 ; 10 is the ASCII code for newline
-len equ $ - msg               ; Calculate the length of the message
+msg db 'Hello, Holberton', 10, 0  ; Null-terminated string with newline
 
 section .text
-global _start                 ; Entry point for the program
+global _start
+extern printf
 
 _start:
-; Call printf function
-mov rdi, fmt                  ; Format string
-mov rsi, msg                  ; Pointer to the message
-call printf                   ; Call printf function
+; Call printf
+mov rdi, fmt          ; Format string
+mov rsi, msg          ; Message to print
+call printf           ; Call printf
 
 ; Exit program
-mov rax, 60                   ; sys_exit system call
-xor rdi, rdi                  ; Exit code 0
-syscall                       ; Invoke syscall
+mov rax, 60           ; sys_exit system call
+xor rdi, rdi          ; Exit code 0
+syscall               ; Invoke syscall
 
 section .data
-fmt db '%s', 0                ; Format string for printf
+fmt db '%s', 0        ; Format string for printf
