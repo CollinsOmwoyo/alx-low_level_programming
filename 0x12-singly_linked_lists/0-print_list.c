de0d6f6cd90adaa3bf346c55010c5a14b64a3896
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-08-07 10:55:18 +0300

    "Replace custom string formatting with printf; update includes"

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * print_list - prints all elements of a list_t list.
@@ -10,35 +10,16 @@
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
+
 while (h)
 {
 if (h->str == NULL)
 {
-_putchar('[');
-_putchar('0');
-_putchar(']');
-_putchar(' ');
-_putchar('(');
-_putchar('n');
-_putchar('i');
-_putchar('l');
-_putchar(')');
-_putchar('\n');
+printf("[0] (nil)\n");
 }
 else
 {
-char buffer[11];
-int len = 0;
-int num = h->len;
-do {
-buffer[len++] = '0' + (num % 10);
-num /= 10;
-} while (num > 0);
-for (int i = len - 1; i >= 0; i--)
-_putchar(buffer[i]);
-_putchar(' ');
-_putchar(h->str[0]);
-_putchar('\n');
+printf("[%u] %s\n", h->len, h->str);
 }
 h = h->next;
 count++;
