177aa4c1a35ce44be002d9060bea7f81d7ef4075
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-08-07 10:59:38 +0300

    "Refactor add_node function: reorder includes, add parameter comments, and update error handling and memory allocation"

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,35 +1,35 @@
 #include "lists.h"
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the pointer to the head of the list.
+ * @str: string to be added to the new node.
  *
- * @head: Double pointer to the head of the list.
- * @str: String to be added to the new node.
- * Return: Address of the new element, or NULL if it failed.
+ * Return: the address of the new element, or NULL if it failed.
  */
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new_node;
-size_t len;
-
-new_node = (list_t *)malloc(sizeof(list_t));
+char *dup_str;
+new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
-exit(1);
-
-new_node->str = strdup(str);
-if (new_node->str == NULL)
 {
-free(new_node);
-exit(1);
+return (NULL);
 }
 
-for (len = 0; str[len]; len++)
-;
-new_node->len = len;
+dup_str = strdup(str);
+if (dup_str == NULL)
+{
+free(new_node);
+return (NULL);
+}
+new_node->str = dup_str;
+new_node->len = strlen(dup_str);
 new_node->next = *head;
+/*Update the head to point to the new node*/
 *head = new_node;
 
-return new_node;
+return (new_node);
 }
