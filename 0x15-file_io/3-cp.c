24d0eefcac69ba5604ea78119efc2434cd35b73b
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-19 12:40:23 +0300

    "Refactor cp command implementation: extract functions for error handling, file opening, and file copying"

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,72 +3,80 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 1024
-
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: arguments passed to the program
  *
- * Return: 0 on success
+ * Return: 0 on success, or the appropriate error code on failure
  */
+#define BUFFER_SIZE 1024
+
+void print_error_and_exit(const char *msg, int exit_code);
+int open_file(const char *filename, int flags, mode_t mode);
+void copy_file(int file_from, int file_to);
+
 int main(int argc, char *argv[])
 {
 int file_from, file_to;
-ssize_t read_count;
-char buffer[BUFFER_SIZE];
 
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+print_error_and_exit("Usage: cp file_from file_to", 97);
 }
 
-file_from = open(argv[1], O_RDONLY);
-if (file_from == -1)
+file_from = open_file(argv[1], O_RDONLY, 0);
+file_to = open_file(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+copy_file(file_from, file_to);
+
+if (close(file_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+print_error_and_exit("Error: Can't close fd", 100);
 }
 
-file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (file_to == -1)
+if (close(file_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-exit(99);
+print_error_and_exit("Error: Can't close fd", 100);
 }
+
+return 0;
+}
+
+void print_error_and_exit(const char *msg, int exit_code)
+{
+dprintf(STDERR_FILENO, "%s\n", msg);
+exit(exit_code);
+}
+
+int open_file(const char *filename, int flags, mode_t mode)
+{
+int fd = open(filename, flags, mode);
+if (fd == -1)
+{
+if (flags & O_RDONLY)
+print_error_and_exit("Error: Can't read from file", 98);
+else
+print_error_and_exit("Error: Can't write to file", 99);
+}
+return fd;
+}
+
+void copy_file(int file_from, int file_to)
+{
+char buffer[BUFFER_SIZE];
+ssize_t read_count;
 
 while ((read_count = read(file_from, buffer, BUFFER_SIZE)) > 0)
 {
 if (write(file_to, buffer, read_count) != read_count)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-close(file_from);
-close(file_to);
-exit(99);
+print_error_and_exit("Error: Can't write to file", 99);
 }
 }
 
 if (read_count == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-close(file_from);
-close(file_to);
-exit(98);
+print_error_and_exit("Error: Can't read from file", 98);
 }
-
-if (close(file_from) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-exit(100);
-}
-
-if (close(file_to) == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-exit(100);
-}
-
-return (0);
 }
