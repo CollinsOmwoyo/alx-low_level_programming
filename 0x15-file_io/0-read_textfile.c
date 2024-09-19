61d4eaab29dceafc244513e550774caa3817c71c
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-19 11:39:28 +0300

    "Update function description and comments in read_textfile function"

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,11 +4,11 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads text file and prints it.
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
  *
- * Return: The actual number of letters it could read and print, or 0 on failure.
+ * Return: actual number of letters, or 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
