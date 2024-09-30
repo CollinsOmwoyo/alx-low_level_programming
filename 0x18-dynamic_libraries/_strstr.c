b0f5a1a7d010d98db538dbda1b1952576ea86092
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-30 23:03:36 +0300

    "Added implementation of _strstr function to locate a substring"

---
+++ 0x18-dynamic_libraries/_strstr.c
@@ -0,0 +1,19 @@
+char *_strstr(char *haystack, char *needle)
+{
+/* Locates a substring */
+char *h, *n;
+while (*haystack)
+{
+h = haystack;
+n = needle;
+while (*h && *n && (*h == *n))
+{
+h++;
+n++;
+}
+if (!*n)
+return haystack;
+haystack++;
+}
+return NULL;
+}
