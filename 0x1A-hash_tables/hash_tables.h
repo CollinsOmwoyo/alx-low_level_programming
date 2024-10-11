ab3bfdaf5f3e8f81c2bc2197b11e081c2f4a3b8e
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-10-11 22:38:13 +0300

    "pROJECT set up"

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,13 @@
+# Hash Table Implementation in C
+using the djb2 hashing algorithm. The hash table uses linked lists to handle collisions (chaining method).
+
+## Files
+
+- **hash_tables.h**: Definitions of data structures and function prototypes.
+- **0-hash_table_create.c**: Creates a new hash table.
+- **1-djb2.c**: Implements the djb2 hashing algorithm.
+- **2-key_index.c**: Retrieves the index for a key.
+- **3-hash_table_set.c**: Adds an element to the hash table.
+- **4-hash_table_get.c**: Retrieves a value associated with a key.
+- **5-hash_table_print.c**: Prints the entire hash table.
+- **6-hash_table_delete.c**: Deletes a hash table.
